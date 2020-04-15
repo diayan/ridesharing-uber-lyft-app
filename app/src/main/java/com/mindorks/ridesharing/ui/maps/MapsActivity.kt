fa74b64d194b8b410f5d322abf1b6a736338d481
diayan <diayans@outlook.com>
2020-04-15 13:07:56 +0100

    presenter.onDetach()

--- app/src/main/java/com/mindorks/ridesharing/ui/maps/MapsActivity.kt
+++ app/src/main/java/com/mindorks/ridesharing/ui/maps/MapsActivity.kt
@@ -5,26 +5,32 @@ import android.os.Bundle
 import android.os.Looper
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.location.LocationCallback
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationResult
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.CameraPosition
-import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.*
 import com.mindorks.ridesharing.R
+import com.mindorks.ridesharing.data.network.NetworkService
+import com.mindorks.ridesharing.utils.MapsUtils
 import com.mindorks.ridesharing.utils.PermissionUtils
 import com.mindorks.ridesharing.utils.ViewUtils
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), MapsView, OnMapReadyCallback {
+
+    companion object {
+        private const val TAG = "MapsActivity"
+        private const val REQUEST_LOCATION_PERMISSION = 1
+    }
 
     private lateinit var googleMap: GoogleMap
-    private val REQUEST_LOCATION_PERMISSION = 1
     private val zoomLevel = 15f
     private lateinit var currentLatLng: LatLng
+    private lateinit var presenter: MapsPresenter
+    private lateinit var locationCallback: LocationCallback
+    private val nearbyCabMarkerList = arrayListOf<Marker>()
+    private var fusedLocationProviderClient: FusedLocationProviderClient? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +39,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        presenter = MapsPresenter(NetworkService())
+        presenter.onAttach(this)
     }
 
     override fun onRequestPermissionsResult(
@@ -93,33 +101,92 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         googleMap.isMyLocationEnabled = true
     }
 
-    private fun animateCamera(latLng: LatLng){
+    private fun animateCamera(latLng: LatLng) {
         val cameraPosition = CameraPosition.Builder().target(latLng).zoom(zoomLevel).build()
         googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
     }
 
+    private fun addCarMarkerAndGet(latLng: LatLng): Marker {
+        return googleMap.addMarker(
+            MarkerOptions().position(latLng).flat(true)
+                .icon(BitmapDescriptorFactory.fromBitmap(MapsUtils.getCabBitmap(this)))
+        )
+    }
+
     private fun setupCurrentLocationListener() {
-        val fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
         val locationRequest = LocationRequest().setInterval(2000).setFastestInterval(2000)
             .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-        fusedLocationProviderClient.requestLocationUpdates(
-            locationRequest,
-            object : LocationCallback() {
-                override fun onLocationResult(locationResult: LocationResult) {
-                    super.onLocationResult(locationResult)
-                    for (location in locationResult.locations) {
-                        currentLatLng = LatLng(location.latitude, location.longitude)
-                        enableMyLocationOnMap()
-                        moveCamera(currentLatLng)
-                        animateCamera(currentLatLng)
-                    }
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                super.onLocationResult(locationResult)
+                for (location in locationResult.locations) {
+                    currentLatLng = LatLng(location.latitude, location.longitude)
+                    enableMyLocationOnMap()
+                    moveCamera(currentLatLng)
+                    animateCamera(currentLatLng)
+                    presenter.requestNearbyCabs(currentLatLng)
                 }
-            },
+            }
+        }
+        fusedLocationProviderClient?.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
             Looper.myLooper()
         )
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         this.googleMap = googleMap
+    }
+
+    override fun showNearbyCabs(latlngList: List<LatLng>) {
+        nearbyCabMarkerList.clear()
+        for (latlng in latlngList) {
+            val nearbyCab = addCarMarkerAndGet(latlng)
+            nearbyCabMarkerList.add(nearbyCab)
+        }
+    }
+
+    override fun informCabBooked() {
+        TODO("Not yet implemented")
+    }
+
+    override fun showPath(latlngList: List<LatLng>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateCabLocation(latLng: LatLng) {
+        TODO("Not yet implemented")
+    }
+
+    override fun informCabIsArriving() {
+        TODO("Not yet implemented")
+    }
+
+    override fun informCabArrived() {
+        TODO("Not yet implemented")
+    }
+
+    override fun informTripStart() {
+        TODO("Not yet implemented")
+    }
+
+    override fun informTripeEnd() {
+        TODO("Not yet implemented")
+    }
+
+    override fun showRoutesNotAvailable() {
+        TODO("Not yet implemented")
+    }
+
+    override fun showDirectionApiFailedError(error: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDestroy() {
+        presenter.onDetach()
+        fusedLocationProviderClient?.removeLocationUpdates(locationCallback)
+        super.onDestroy()
     }
 }
